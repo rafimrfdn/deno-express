d766aa9f9c37af4a62456241768b76ff92bc2f34
rafimrfdn <uphy.mks@gmail.com>
2024-04-01 04:33:05 +0800

    update

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 import express from 'npm:express@4.17';
 
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')))
+//app.use(express.static('public'));
 app.use(express.json());
 
 
