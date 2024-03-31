34e4dc7629cdcd9fda920e1431c9c357edaff357
Muhammad Rafiuddin <71545866+rafimrfdn@users.noreply.github.com>
2024-04-01 03:28:01 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ const app = express();
 
 //app.use(express.static('public'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(_dirname, 'public')));
 
 
 app.use((_, res, next) => {
